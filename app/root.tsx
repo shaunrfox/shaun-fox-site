f5dc3487f9f38f9696be22e9c5ff901f86be76b6
Seth Davis <sethdavis@indeed.com>
2023-01-26 14:31:03 -0600

    Add pico

--- app/root.tsx
+++ app/root.tsx
@@ -1,36 +1,45 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from '@remix-run/node';
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+    Links,
+    LiveReload,
+    Meta,
+    Outlet,
+    Scripts,
+    ScrollRestoration
+} from '@remix-run/react';
 import { Analytics } from '@vercel/analytics/react';
 import Navbar from './components/Navbar';
 
+export const links: LinksFunction = () => {
+    return [
+        {
+            rel: 'stylesheet',
+            href: 'https://unpkg.com/@picocss/pico@latest/css/pico.min.css'
+        }
+    ];
+};
+
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
+    charset: 'utf-8',
+    title: 'New Remix App',
+    viewport: 'width=device-width,initial-scale=1'
 });
 
 export default function App() {
-  return (
-      <html lang="en">
-          <head>
-              <Meta />
-              <Links />
-          </head>
-          <body>
-              <Navbar />
-              <Outlet />
-              <ScrollRestoration />
-              <Scripts />
-              <LiveReload />
-              <Analytics />
-          </body>
-      </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <Meta />
+                <Links />
+            </head>
+            <body>
+                <Navbar />
+                <Outlet />
+                <ScrollRestoration />
+                <Scripts />
+                <LiveReload />
+                <Analytics />
+            </body>
+        </html>
+    );
 }
