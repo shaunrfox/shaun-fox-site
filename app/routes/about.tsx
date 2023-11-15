51c3b8800aa10dc244fb35a64501cd757f10a1fc
Seth Davis <sethdavis512@gmail.com>
2023-11-14 20:37:37 -0600

    Add photo

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -3,6 +3,10 @@ export default function AboutRoute() {
         <>
             <h1>About</h1>
             <p>Hey, I'm Shaun Fox</p>
+            <img
+                src="https://shaunfox.com/assets/fox/shaun1.png"
+                alt="Shaun Fox looking oh so good"
+            />
         </>
     );
 }
