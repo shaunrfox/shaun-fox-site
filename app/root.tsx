ac717bae7b81e0222a19f4d22ff79cb27d3b946f
Seth Davis <sethdavis512@gmail.com>
2023-11-14 20:28:41 -0600

    Upgrade to v2

--- app/root.tsx
+++ app/root.tsx
@@ -27,11 +27,13 @@ export const links: LinksFunction = () => {
     ];
 };
 
-export const meta: MetaFunction = () => ({
-    charset: 'utf-8',
-    title: 'New Remix App',
-    viewport: 'width=device-width,initial-scale=1'
-});
+export const meta: MetaFunction = () => [
+    {
+        charset: 'utf-8',
+        title: 'New Remix App',
+        viewport: 'width=device-width,initial-scale=1'
+    }
+];
 
 export default function App() {
     return (
