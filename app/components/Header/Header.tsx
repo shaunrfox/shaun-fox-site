bc91a5867990d4a4ba98db6bde7310d80914cca1
Shaun Fox <shaunrfox@gmail.com>
2024-08-13 23:42:59 -0500

    New site working on desktop

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,83 @@
+import { useEffect, useRef } from "react";
+import type { LinksFunction } from "@remix-run/node";
+import { Link, useLocation } from "@remix-run/react";
+import headerStyles from "./header.css";
+import Rule from "../Rule";
+import FoxLogo, { links as foxLinks } from "~/components/Foxes/FoxLogo";
+import FoxLogoOutline from "~/components/Foxes/FoxLogoOutline";
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: headerStyles,
+    },
+    ...foxLinks(),
+  ];
+};
+
+export function Navbar() {
+  const { pathname } = useLocation();
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          {pathname === "/" ? <span>Home</span> : <Link to="/">Home</Link>}
+        </li>
+        <li>
+          {pathname === "/cv" ? <span>CV</span> : <Link to="/cv">CV</Link>}
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default function Header(): JSX.Element {
+  const logoRef = useRef<HTMLDivElement>(null);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const setLogoHeight = () => {
+      if (logoRef.current) {
+        const docHeight =
+          Math.max(
+            document.body.offsetHeight,
+            document.documentElement.clientHeight,
+            document.documentElement.offsetHeight
+          ) - 40;
+        logoRef.current.style.height = `${docHeight}px`;
+      }
+    };
+
+    setLogoHeight();
+    window.addEventListener("resize", setLogoHeight);
+
+    // Re-check on navigation
+    setLogoHeight();
+
+    return () => {
+      window.removeEventListener("resize", setLogoHeight);
+    };
+  }, [pathname]);
+  return (
+    <header>
+      <div className="header-wrapper">
+        <div className="logo" ref={logoRef}>
+          <Link to="/">
+            <FoxLogoOutline />
+            <FoxLogo />
+            <div className="circle"></div>
+          </Link>
+        </div>
+        <div className="header-content">
+          <div className="name">Shaun Fox</div>
+          <div>Principal Product Designer</div>
+          <span>Austin, Texas</span>
+        </div>
+        <Navbar />
+      </div>
+      <Rule thickness={4} />
+    </header>
+  );
+}
--- app/root.tsx
+++ app/root.tsx
@@ -1,58 +1,55 @@
-import type { LinksFunction, MetaFunction } from '@remix-run/node';
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
-    Links,
-    LiveReload,
-    Meta,
-    Outlet,
-    Scripts,
-    ScrollRestoration
-} from '@remix-run/react';
-import { Analytics } from '@vercel/analytics/react';
-import Footer from './components/Footer';
-import Header from './components/Header';
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import { Analytics } from "@vercel/analytics/react";
+import Header, { links as headerLinks } from "./components/Header";
+import Footer, { links as footerLinks } from "./components/Footer/Footer";
 
-import resetStyles from './styles/reset.css';
-import globalStyles from './styles/global.css';
+import globalStyles from "~/styles/global.css";
 
 export const links: LinksFunction = () => {
-    return [
-        {
-            rel: 'stylesheet',
-            href: resetStyles
-        },
-        {
-            rel: 'stylesheet',
-            href: globalStyles
-        }
-    ];
+  return [
+    {
+      rel: "stylesheet",
+      href: globalStyles,
+    },
+    ...headerLinks(),
+    ...footerLinks(),
+  ];
 };
 
 export const meta: MetaFunction = () => [
-    {
-        charset: 'utf-8',
-        title: 'New Remix App',
-        viewport: 'width=device-width,initial-scale=1'
-    }
+  {
+    charset: "utf-8",
+    title: "New Remix App",
+    viewport: "width=device-width,initial-scale=1",
+  },
 ];
 
 export default function App() {
-    return (
-        <html lang="en">
-            <head>
-                <Meta />
-                <Links />
-            </head>
-            <body>
-                <Header />
-                <main className="container">
-                    <Outlet />
-                </main>
-                <Footer />
-                <ScrollRestoration />
-                <Scripts />
-                <LiveReload />
-                <Analytics />
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Header />
+        <main className="container">
+          <Outlet />
+        </main>
+        <Footer />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+        <Analytics />
+      </body>
+    </html>
+  );
 }
